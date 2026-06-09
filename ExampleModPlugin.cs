01d100a1e5d08c9c6cd42d95fa3b13f2f6c595e0
Jakob Hellermann <jakob.hellermann@protonmail.com>
2026-06-09 19:29:49 +0200

    Apply formatting

--- ExampleModPlugin.cs
+++ ExampleModPlugin.cs
@@ -3,11 +3,9 @@ using BepInEx;
 namespace ExampleMod;
 
 // TODO - adjust the plugin guid as needed
-[BepInAutoPlugin(id: "io.github.yourgithubusername.examplemod")]
-public partial class ExampleModPlugin : BaseUnityPlugin
-{
-    private void Awake()
-    {
+[BepInAutoPlugin("io.github.yourgithubusername.examplemod")]
+public partial class ExampleModPlugin : BaseUnityPlugin {
+    private void Awake() {
         // Put your initialization logic here
         Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
     }
