f58acd08a9912c2f4d8f78dd44d356cc2f3171c7
Jakob Hellermann <jakob.hellermann@protonmail.com>
2026-06-09 19:02:26 +0200

    Setup harmony

--- Source/ExampleModPlugin.cs
+++ Source/ExampleModPlugin.cs
@@ -1,19 +1,34 @@
+using System;
 using BepInEx;
+using HarmonyLib;
 
 namespace ExampleMod.Source;
 
 // TODO - adjust the plugin guid as needed
 [BepInAutoPlugin("io.github.yourgithubusername.examplemod")]
 public partial class ExampleModPlugin : BaseUnityPlugin {
+    private Harmony harmony = null!;
+
     private void Awake() {
         Log.Init(Logger);
-
-        // Put your initialization logic here
         Log.Info($"Plugin {Name} ({Id}) has loaded!");
+
+        try {
+            harmony = Harmony.CreateAndPatchAll(GetType().Assembly);
+        } catch (Exception e) {
+            Log.Info($"Plugin {Name} ({Id}) failed to initialize: {e}");
+        }
     }
 
     private void OnDestroy() {
         // Clean up everything, in order to support hot reloading
+
+        try {
+            harmony.UnpatchSelf();
+        } catch (Exception e) {
+            Log.Info($"Plugin {Name} ({Id}) failed to clean up: {e}");
+        }
+
         Log.Info($"Plugin {Name} ({Id}) has been unloaded!");
     }
 }
